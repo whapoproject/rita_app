18463ca9808dc7fa85316557efcf9d59312f3430
whapoproject <whapoproject@gmail.com>
2026-05-26 11:50:09 -0400

    add working sitemap

---
+++ chat/sitemap.py
@@ -0,0 +1,17 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+
+class StaticViewSitemap(Sitemap):
+    priority = 0.8
+    changefreq = "daily"
+
+    def items(self):
+        return [
+            'welcome',
+            'login',
+            'register',
+        ]
+
+    def location(self, item):
+        return reverse(item)
--- privatechat/urls.py
+++ privatechat/urls.py
@@ -18,10 +18,22 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from chat.sitemap import StaticViewSitemap
+
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('chat.urls')),
+    path(
+    'sitemap.xml',
+    sitemap,
+    {'sitemaps': sitemaps},
+    name='django.contrib.sitemaps.views.sitemap'
+),
 ] 
 
 if settings.DEBUG:
